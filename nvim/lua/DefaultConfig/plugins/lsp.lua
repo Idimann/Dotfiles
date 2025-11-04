f6a2a91d93baab663f75370856fd0c6092c07762
lolfuck <werner.sprey@gmail.com>
2025-11-04 12:46:27 +0100

    Added wofi

--- nvim/lua/DefaultConfig/plugins/lsp.lua
+++ nvim/lua/DefaultConfig/plugins/lsp.lua
@@ -1,29 +1,25 @@
-local lspconfig = require('lspconfig')
 local cap = require('blink.cmp').get_lsp_capabilities()
 
-lspconfig.clangd.setup({ capabilities = cap })
-lspconfig.lua_ls.setup({ capabilities = cap })
-lspconfig.pylsp.setup({ capabilities = cap })
-lspconfig.rust_analyzer.setup({ capabilities = cap })
-lspconfig.gopls.setup({ capabilities = cap })
-lspconfig.ts_ls.setup({ capabilities = cap })
-lspconfig.zls.setup({ capabilities = cap})
-vim.g.zig_fmt_autosave = false
-lspconfig.hls.setup({ capabilities = cap })
-lspconfig.elixirls.setup({
+vim.lsp.config('*', { capabilities = cap })
+vim.lsp.config.elixirls = {
     capabilities = cap,
     cmd = { "/usr/lib/elixir-ls/language_server.sh" },
+}
+vim.lsp.enable({ 'lua_ls',
+    'rust_analyzer',
+    'elixirls',
+    'clangd',
+    'zls',
 })
-lspconfig.gleam.setup({ capabilities = cap })
--- lspconfig.htmx.setup({ capabilities = cap })
 
-vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.hover({border = 'single'}) end);
+vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.hover({ border = 'single' }) end);
 vim.keymap.set("n", "<leader>j", function()
-    vim.diagnostic.open_float({border = 'single'}) end);
--- vim.keymap.set("n", "<leader>l", vim.lsp.buf.format);
--- vim.keymap.set("n", "<leader>d", vim.diagnostic.setqflist);
--- vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition);
--- vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration);
+    vim.diagnostic.open_float({ border = 'single' })
+end);
+vim.keymap.set("n", "<leader>l", vim.lsp.buf.format);
+vim.keymap.set("n", "<leader>d", vim.diagnostic.setqflist);
+vim.keymap.set("n", "gd", vim.lsp.buf.definition);
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration);
 -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename);
 -- vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references);
 -- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action);
